e8f8fea48b92f76f5064851eb05a69c38d0fbfe5
arakichan-XD <itsmearakichan@gmail.com>
2025-10-25 18:26:31 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -8,10 +8,14 @@ export default defineConfig(({ mode }) => {
     server: {
       port: 3000,
       host: '0.0.0.0',
+      allowedHosts: 'all',
+      disableHostCheck: true
     },
     preview: {
       port: process.env.PORT || 3000,
       host: '0.0.0.0',
+      allowedHosts: 'all',
+      disableHostCheck: true
     },
     plugins: [react()],
     define: {
